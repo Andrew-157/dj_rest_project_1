c564719b915bb2052b11e8689d0bb541e088bfa7
Andrew Subotin <subotinandrey5@gmail.com>
2023-05-15 10:09:09 +0300

    Registered djoser and customized djoser serializers

--- cucumber/urls.py
+++ cucumber/urls.py
@@ -21,6 +21,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
+
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
