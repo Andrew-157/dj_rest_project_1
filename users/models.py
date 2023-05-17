d2fbb247af685c046123bbf22b50c21b09ca7269
Andrew Subotin <subotinandrey5@gmail.com>
2023-05-17 10:35:13 +0300

    Changed Meta Class of CustomUser to add default ordering by id

--- users/models.py
+++ users/models.py
@@ -4,3 +4,6 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     user_image = models.ImageField(upload_to='users/images')
+
+    class Meta:
+        ordering = ['id']
